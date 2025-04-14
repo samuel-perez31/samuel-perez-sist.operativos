6b59eb2c8fb322f2cc3063a2fce7e73a34a8f0a0
samuel-perez31 <samuperezmorabito@gmail.com>
2025-04-14 09:45:26 +0200

    Update 25-4-14.py

--- 25-4-14.py
+++ 25-4-14.py
@@ -50,6 +50,42 @@ kilometros = 1.609344 * millas
 
 print("La cantidad de millas en kilometros es: ",kilometros)
 
-"""
+
 #7
 
+lb = float(input("Ingrese lado b: "))
+lc = float(input("Ingresa lado c: "))
+a = float(input("Ingresa ángulo alfa: "))
+
+r = a * 3.1416 / 180
+
+la = lb ** 2 + lc ** 2 - 2*lb*lc * math.cos(r) ** 0.5
+
+
+#8
+
+va = float(input("Ingrese velocidad a: "))
+vb = float(input("Ingrese velocidad b: "))
+d = float(input("Ingrese distancia: "))
+
+te = d/(va+vb)
+
+print(te)
+
+#9
+angulor = int(input("Ingrese el angulo en radianes: "))
+sex = angulor * 180 / 3.1416
+cen = angulor * 200 / 3.1416
+print("En sexagesimal es: ", sex)
+print("En cen es: ", cen)
+"""
+#10
+x1 = int(input("Ingrese el valor de x1: "))
+y1 = int(input("Ingrese el valor de y1: "))
+z1 = int(input("Ingrese el valor de z1: "))
+x2 = int(input("Ingrese el valor de x2: "))
+y2 = int(input("Ingrese el valor de y2: "))
+z2 = int(input("Ingrese el valor de z2: "))
+
+distancia = ((z2-z1)**2+(y2-y1)**2+(x2-x1)**2)**0.5
+print("La distancia es: ",distancia)
