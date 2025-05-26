d2798db4aa6f3c942d8812ccd488dfb96eeb6e83
samuel-perez31 <samuperezmorabito@gmail.com>
2025-05-26 10:54:33 -0300

    Delete 25-5-26 directory

--- 25-5-26/25-5-26/3.py
+++
@@ -1,9 +0,0 @@
-vasosdia = 0
-dia = 0
-while dia < 7:
-    vasos = int(input("Cuantos vasos tomaste hoy?: "))
-    vasosdia += vasos
-    dia += 1
-print("La cantidad de vasos por dia es: ", round(vasosdia/7,2))
-if vasosdia/7 < 8:
-    print("Deberias tomar mas agua")
